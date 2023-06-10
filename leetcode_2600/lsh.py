f9eacbbf37c6952bf5af0c5b521291a0a6536dc5
LSH <myomyo1912@gmail.com>
2023-06-10 19:11:47 +0900

    leetcode 2600 python

---
+++ leetcode_2600/lsh.py
@@ -0,0 +1,19 @@
+"""
+2600. K Items With the Maximum Sum
+Lee SooHyung
+"""
+
+class Solution:
+    def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
+        if numOnes >= k:
+            return k
+        elif numOnes + numZeros >= k:
+            return numOnes
+        else:
+            return numOnes - (k - (numOnes + numZeros))
+
+'''
+RESULT
+Runtime 42ms, 71.61% beats
+Memory 16.3MB, 53.42% beats
+'''
