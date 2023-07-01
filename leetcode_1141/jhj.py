7edb85ff7ec497293c89aff48141cdbb40bfc19e
Alchera-HyeonjiJeong <129029872+Hyeonji-Jeong@users.noreply.github.com>
2023-07-01 23:36:59 +0900

    [jhj] leetcode 66, 724, 1141

---
+++ leetcode_1141/jhj.py
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT activity_date AS day, COUNT(DISTINCT(user_id)) AS active_users
+FROM Activity
+WHERE DATEDIFF('2019-07-27', activity_date) < 30 AND DATEDIFF('2019-07-27', activity_date)>=0
+GROUP BY activity_date
+
+"""
+Runtime: 1159 ms
+Memory Usage: 0B
+"""
